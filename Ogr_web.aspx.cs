70915666e28833e2e6961203b3d1288a60c5c51e
pinarkirbasoglu <99248927+pinarkirbasoglu@users.noreply.github.com>
2023-04-25 12:40:13 +0300

    ortalama karşılaştırma 1.yol

---
+++ Ogr_web.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.VisualBasic;
+
+namespace Ortalama_Karsilastirma
+{
+    public partial class Ogr_web : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Ogrenci ogrenci = new Ogrenci();
+            string[] isim = new string[2];
+            int[] not = new int[3];
+            int[] ortalama = new int[2];
+            for(int i = 0;i < isim.Length; i++)
+            {
+                isim[i] = Interaction.InputBox((i + 1) + ".öğrencinin ismini girin:");
+            }
+            for(int i = 0; i<2; i++)
+            {
+                for(int j = 0; j<not.Length; j++)
+                {
+                    not[j]=Convert.ToInt32(Interaction.InputBox((i+1)+".öğrencinin"+" " +(j+1)+". notunu giriniz:"));
+                }
+                ogrenci.Notlar = not;//önce aspx deki not dizisine notlar alıpnıp bu dizi cs deki notlar dizisine kayıtedilmeli bunu yapmadan ort_hesapla çağırılmaz çünkü metot cs deki notlar dizisinin elemanalrını toplayıp bölerek ortlama buluyor.
+                ogrenci.ort_hesapla(i);//öğrenci nesensin isim dizi ile ortalama dizinde ki uyumu cs dosyasında sıra metotdunda ÖĞRENCİ NESENSİNİ İ.ELEMANI DİZİLERDE İ.İNDEXDE TUTULMALI prensibi ile sağladık.
+            }
+
+            Response.Write(ogrenci.sırala());
+        }
+    }
+}
---
+++ Ogrenci.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ortalama_Karsilastirma
+{
+    public class Ogrenci
+    {
+        private string[] isimler = new string[2];
+        private int[] notlar = new int[3];
+        private double[] ortalama = new double[2];
+
+        public string[] Isimler { get => isimler; set => isimler = value; }
+        public int[] Notlar { get => notlar; set => notlar = value; }
+        public double[] Ortalama { get => ortalama; set => ortalama = value; }
+
+        public void ort_hesapla(int x)//int x dediğimiz öğrenci isminin dizideki indexini alacak.
+        {
+            int top = 0;
+            foreach(int not in Notlar)
+            {
+                top += not;
+            }
+            Ortalama[x] = top / 3;/*öğrenci nesenesinin isim dizisi ile ortalama dizisinin uyumlu olması için;
+            isim dizindeki oda numarasında(index) de tutulan bilgi ortlama dizisinde de aynı oda numarasında(indexinde) tutarak uyum 
+            sağlanır.Biz i.elemanın bilgilerine uyumlu şekilde erişelim.
+            *** ÖĞRENCİ NESENESİNİN İ. ELEMANINA AİT BİLGİLER TÜM DİZİLERDE İ.ODA NUMARASINDA(İNDEXİNDE) TUTULMALI */
+        }/*ort_hesapla metodu öğrenci nesenesinin ortlama özelliğinini doldurur.*/
+
+
+        public string sırala()
+        {
+            int eboi = 0;
+            int ekoi = 0;
+            double ebo = Ortalama[0];
+            double eko = Ortalama[0];
+            for (int i =0; i < 2; i++)//eb ek nin dizideki indexini bulup o indexi diziden çağırarak kullandık eb ek olan öğrenciyi.
+            {
+                if (ebo < Ortalama[i]) eboi = i;
+
+                if (eko > Ortalama[i]) ekoi = i;
+            }
+            string sonuc = Isimler[eboi] + " isimli öğrenci" + Ortalama[eboi] + " ortalma ile en yüksek ortlamaya sahiptir<br>" +
+                Isimler[ekoi] + " isimli öğrenci" + Ortalama[ekoi] + " ortalma ile en düşük ortlamaya sahiptir<br>";
+            return sonuc;
+        }
+    }
+}
